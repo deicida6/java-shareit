7ccec373338ad9fd5dccb8130c2113192b4914ad
Andrey Zhirnov <deicida6@yandex.ru>
2024-07-20 13:54:43 +0300

    Sprint_15_add-bookings fix#3

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -19,7 +19,6 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
     @Column(name = "name", nullable = false)
     private String name;
